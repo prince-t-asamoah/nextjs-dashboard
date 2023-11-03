4b8e370f6cef13dc3617b34e58f5d003fad95858
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-03 10:59:00 +0000

    add hero image for mobile and desktop

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import AcmeLogo from '@/app/ui/acme-logo';
 import Link from 'next/link';
+import Image from 'next/image';
+import AcmeLogo from '@/app/ui/acme-logo';
 import { lusitana } from './ui/fonts';
 
 export default function Page() {
@@ -32,7 +33,20 @@ export default function Page() {
                     </Link>
                 </div>
                 <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-                    {/* Add Hero Images Here */}
+                    <Image
+                        src="/hero-desktop.png"
+                        alt="Screenshots of the dashboard project showing desktop and mobile versions"
+                        width={1000}
+                        height={760}
+                        className="hidden md:block"
+                    />
+                    <Image
+                        src="/hero-mobile.png"
+                        alt="Screenshots of the dashboard project showing desktop and mobile versions"
+                        width={560}
+                        height={620}
+                        className="md:hidden"
+                    />
                 </div>
             </div>
         </main>
