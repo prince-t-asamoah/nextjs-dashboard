de58a79cb6227c22a24359caeb297c9f399e5092
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-06 08:54:44 +0000

    fetch and show dashboard card data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
-import { fetchRevenue, fetchLatestInvoices } from '../lib/data';
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data';
+import { Card } from '../ui/dashboard/cards';
 import { lusitana } from '../ui/fonts';
 import RevenueChart from '../ui/dashboard/revenue-chart';
 import LatestInvoices from '../ui/dashboard/latest-invoices';
@@ -6,6 +7,7 @@ import LatestInvoices from '../ui/dashboard/latest-invoices';
 export default async function DashboardPage() {
     const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
+    const cardData = await fetchCardData();
 
     return (
         <main>
@@ -13,14 +15,26 @@ export default async function DashboardPage() {
                 Dashboard
             </h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-                {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-                {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-                {/* <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
+                <Card
+                    title="Collected"
+                    value={cardData.totalPaidInvoices}
+                    type="collected"
+                />
+                <Card
+                    title="Pending"
+                    value={cardData.totalPendingInvoices}
+                    type="pending"
+                />
+                <Card
+                    title="Total Invoices"
+                    value={cardData.numberOfInvoices}
+                    type="invoices"
+                />
+                <Card
+                    title="Total Customers"
+                    value={cardData.numberOfCustomers}
+                    type="customers"
+                />
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
                 <RevenueChart revenue={revenue} />
