379352f34060a964ba49b7b24dd0f82af200e61e
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-07 11:26:00 +0000

    create edit invoice page

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,3 +1,31 @@
-export default async function EditInvoicePage() {
-    return <div>EditPage</div>;
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+
+export default async function EditInvoicePage({
+    params,
+}: {
+    params: { id: string };
+}) {
+    const id = params.id ?? "";
+    const [invoice, customers] = await Promise.all([
+        fetchInvoiceById(id),
+        fetchCustomers(),
+    ]);
+
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: 'Invoices', href: '/dashboard/invoices' },
+                    {
+                        label: 'Edit Invoice',
+                        href: `/dashboard/invoices/${id}/edit`,
+                        active: true,
+                    },
+                ]}
+            />
+            <Form invoice={invoice} customers={customers} />
+        </main>
+    );
 }
