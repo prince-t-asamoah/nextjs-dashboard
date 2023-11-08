81b767fc80d34d20931f8d431b317341dd02b73c
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-08 15:05:21 +0000

    add page breadcrumb navigation

--- app/dashboard/customers/add/page.tsx
+++ app/dashboard/customers/add/page.tsx
@@ -1,3 +1,18 @@
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+
 export default function AddCustomersPage() {
-    return <div>Add customers page</div>;
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: 'Customers', href: '/dashboard/customers' },
+                    {
+                        label: 'Add customers',
+                        href: '/dashboard/customers/add',
+                        active: true,
+                    },
+                ]}
+            />
+        </main>
+    );
 }
