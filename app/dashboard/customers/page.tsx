ecb330c4f4db8f2d7d9eb7e0325f0bd3b6928119
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-08 16:12:33 +0000

    use add customer button component

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,4 @@
+import { AddCustomerButton } from '@/app/ui/customers/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
 
@@ -9,6 +10,7 @@ export default function CustomersPage() {
             </div>
             <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
                 <Search placeholder="Search customers..." />
+                <AddCustomerButton />
             </div>
         </div>
     );
