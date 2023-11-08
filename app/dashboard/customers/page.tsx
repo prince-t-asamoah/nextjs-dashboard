80c6951118cc03bcccbd22119806e63a1d75239c
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-08 14:51:35 +0000

    add heading and search input

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,15 @@
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+
 export default function CustomersPage() {
-    return <p>Customers Page</p>;
+    return (
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+            </div>
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                <Search placeholder="Search customers..." />
+            </div>
+        </div>
+    );
 }
