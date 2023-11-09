e2616a22a03f10a4b018aded6403ca5a095747ed
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-09 11:13:20 +0000

    update invoice page component prop

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,24 +1,23 @@
 import { Suspense } from 'react';
 import { Metadata } from 'next';
+
+import { fetchInvoicesPages } from '@/app/lib/data';
+import { PageSearchParams } from '@/app/lib/definitions';
+
 import Pagination from '@/app/ui/pagination';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-import { fetchInvoicesPages } from '@/app/lib/data';
 
 export const metadata: Metadata = {
     title: 'Invoices',
 };
 
-export default async function InvoicesPage({
-    searchParams,
-}: {
-    searchParams?: { query?: string; page?: string };
-}) {
+export default async function InvoicesPage({ searchParams }: PageSearchParams) {
     const query = searchParams?.query ?? '';
-    const currentPage = Number(searchParams?.page ?? 1);
+    const currentPage = Number(searchParams?.page ?? '1');
     const totalPages = await fetchInvoicesPages(query);
 
     return (
