685892ac20feaab4f481315a48b4f24b8dec6468
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-09 12:16:09 +0000

    implement upload image functionality

--- app/ui/customers/upload-image.tsx
+++ app/ui/customers/upload-image.tsx
@@ -1,12 +1,33 @@
 'use client';
 
 import { UserCircleIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
+import Image from 'next/image';
 
 export default function UploadImage() {
+    const [imageURL, setImageURL] = useState('');
+
+    const handleInputOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            const url = URL.createObjectURL(e.target.files[0]);
+            setImageURL(url);
+        }
+    };
+
     return (
         <div className="flex flex-col">
-            <div className="w-40 h-40 bg-white rounded-full">
-                <UserCircleIcon className="text-gray-600" />
+            <div className="w-40 h-40 bg-white rounded-full flex justify-center items-center">
+                {imageURL ? (
+                    <Image
+                        src={imageURL}
+                        alt="Profile image"
+                        width={160}
+                        height={160}
+                        className="w-full h-full rounded-full object-cover"
+                    />
+                ) : (
+                    <UserCircleIcon className="text-gray-600" />
+                )}
             </div>
             <label
                 htmlFor="image_url"
@@ -20,6 +41,7 @@ export default function UploadImage() {
                 type="file"
                 className="hidden"
                 aria-describedby="image-url-error"
+                onChange={handleInputOnChange}
             />
         </div>
     );
