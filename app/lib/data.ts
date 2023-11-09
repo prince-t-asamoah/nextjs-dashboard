13f9075caceb392ad45c4e8587a18173f63c3252
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-09 08:57:49 +0000

    update fetchCustomers db query

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,6 +1,5 @@
 import { sql } from '@vercel/postgres';
 import {
-    CustomerField,
     CustomersTable,
     InvoiceForm,
     InvoicesTable,
@@ -173,18 +172,32 @@ export async function fetchInvoiceById(id: string) {
     }
 }
 
-export async function fetchCustomers() {
+export async function fetchCustomers(query = '') {
     noStore();
     try {
-        const data = await sql<CustomerField>`
-      SELECT
-        id,
-        name
-      FROM customers
-      ORDER BY name ASC
-    `;
+        const data = await sql<CustomersTable>`
+		SELECT
+		  customers.id,
+		  customers.name,
+		  customers.email,
+		  customers.image_url,
+		  COUNT(invoices.id) AS total_invoices,
+		  SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
+		  SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
+		FROM customers
+		LEFT JOIN invoices ON customers.id = invoices.customer_id
+		WHERE
+		  customers.name ILIKE ${`%${query}%`} OR
+        customers.email ILIKE ${`%${query}%`}
+		GROUP BY customers.id, customers.name, customers.email, customers.image_url
+		ORDER BY customers.name ASC
+	  `;
 
-        const customers = data.rows;
+        const customers = data.rows.map((customer) => ({
+            ...customer,
+            total_pending: formatCurrency(customer.total_pending),
+            total_paid: formatCurrency(customer.total_paid),
+        }));
         return customers;
     } catch (err) {
         console.error('Database Error:', err);
