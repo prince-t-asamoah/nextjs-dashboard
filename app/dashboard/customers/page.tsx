bbe9c516496b553658e9547494b1ad79763427b2
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-09 10:55:37 +0000

    show page pagination

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,12 +1,18 @@
-import { fetchCustomers, fetchFilteredCustomers } from '@/app/lib/data';
+import { fetchCustomersTotal, fetchFilteredCustomers } from '@/app/lib/data';
+import { PageSearchParams } from '@/app/lib/definitions';
+
 import { AddCustomerButton } from '@/app/ui/customers/buttons';
 import CustomersTable from '@/app/ui/customers/table';
 import { lusitana } from '@/app/ui/fonts';
 import Pagination from '@/app/ui/pagination';
 import Search from '@/app/ui/search';
 
-export default async function CustomersPage() {
-    const customers = await fetchCustomers();
+export default async function CustomersPage({
+    searchParams,
+}: PageSearchParams) {
+    const query = searchParams?.query ?? '';
+    const customers = await fetchFilteredCustomers(query);
+    const totalPages = await fetchCustomersTotal();
 
     return (
         <div className="w-full">
@@ -14,12 +20,12 @@ export default async function CustomersPage() {
                 <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
             </div>
             <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-                <Search placeholder="Search customers..." />
+                <Search placeholder="Search customers by name or email" />
                 <AddCustomerButton />
             </div>
             <CustomersTable customers={customers} />
             <div className="mt-5 flex w-full justify-center">
-                <Pagination totalPages={0} />
+                <Pagination totalPages={totalPages} />
             </div>
         </div>
     );
