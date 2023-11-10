bd1d78753bcd00c4dda1d0d212e271608f904754
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-10 14:21:53 +0000

    manage form state using add customer action

--- app/ui/customers/add-form.tsx
+++ app/ui/customers/add-form.tsx
@@ -1,19 +1,30 @@
+'use client';
+
+import { useFormState } from 'react-dom';
 import { AtSymbolIcon } from '@heroicons/react/20/solid';
 import { UserCircleIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+
+import { addCustomers } from '@/app/lib/actions';
 import { Button } from '../button';
 import UploadImage from './upload-image';
+import ErrorMessage from '../error-message';
 
 export default function AddCustomerForm() {
+    const [state, action] = useFormState(addCustomers, {
+        errors: {},
+        message: '',
+    });
+
     return (
-        <form>
+        <form action={action}>
             <div className="rounded-md bg-gray-50 p-4 md:p-6">
                 <div className="mb-4 flex justify-center">
                     <UploadImage />
                 </div>
                 <div className="mb-4">
                     <label
-                        htmlFor="name"
+                        htmlFor="fullName"
                         className="mb-2 block text-sm font-medium"
                     >
                         Name
@@ -21,16 +32,19 @@ export default function AddCustomerForm() {
                     <div className="relative">
                         <div className="relative">
                             <input
-                                id="name"
-                                name="name"
+                                id="fullName"
+                                name="fullName"
                                 type="text"
                                 placeholder="Enter fullname"
                                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-                                aria-describedby="name-error"
+                                aria-describedby="fullName-error"
                             />
                             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
                         </div>
                     </div>
+                    {state.errors?.fullName && (
+                        <ErrorMessage id="fullName" message={state.errors.fullName[0]} />
+                    )}
                 </div>
                 <div className="mb-4">
                     <label
@@ -52,7 +66,11 @@ export default function AddCustomerForm() {
                             <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
                         </div>
                     </div>
+                    {state.errors?.email && (
+                        <ErrorMessage id="email" message={state.errors.email[0]} />
+                    )}
                 </div>
+                {state?.message && <ErrorMessage message={state.message} />}
             </div>
             <div className="mt-6 flex justify-end gap-4">
                 <Link
