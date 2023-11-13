873e797a304d7e96d7702f48b5e7784ddcb8fecc
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-13 11:06:46 +0000

    add breadcrumb and page title

--- app/dashboard/customers/[id]/edit/page.tsx
+++ app/dashboard/customers/[id]/edit/page.tsx
@@ -1,3 +1,29 @@
-export default function EditCustomersPage() {
-    return <div>Edit customer page</div>;
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Edit customer',
+};
+
+export default function EditCustomersPage({
+    params,
+}: {
+    params: { id: string };
+}) {
+    const id = params.id ?? '';
+
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: 'Customers', href: '/dashboard/customers' },
+                    {
+                        label: 'Edit customer',
+                        href: `/dashboard/customers/${id}/edit`,
+                        active: true,
+                    },
+                ]}
+            />
+        </main>
+    );
 }
