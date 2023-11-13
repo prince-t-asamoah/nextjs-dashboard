2107a58828fb9d3a788fa88c3707d918eb86046e
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-13 10:31:29 +0000

    add update and delete customer button

--- app/ui/customers/buttons.tsx
+++ app/ui/customers/buttons.tsx
@@ -1,5 +1,6 @@
-import { PlusIcon } from '@heroicons/react/24/outline';
+import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { deleteInvoice } from '@/app/lib/actions';
 
 export function AddCustomerButton() {
     return (
@@ -10,5 +11,28 @@ export function AddCustomerButton() {
             <PlusIcon className="h-5 md:mr-4" />
             <span className="hidden md:block">Add customers</span>
         </Link>
+    );
+}
+
+export function UpdateCustomer({ id }: { id: string }) {
+    return (
+        <Link
+            href={`/dashboard/customers/${id}/edit`}
+            className="rounded-md border p-2 hover:bg-gray-100"
+        >
+            <PencilIcon className="w-5" />
+        </Link>
+    );
+}
+
+export function DeleteCustomer({ id }: { id: string }) {
+    const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+    return (
+        <form action={deleteInvoiceWithId}>
+            <button className="rounded-md border p-2 hover:bg-gray-100">
+                <span className="sr-only">Delete</span>
+                <TrashIcon className="w-5" />
+            </button>
+        </form>
     );
 }
