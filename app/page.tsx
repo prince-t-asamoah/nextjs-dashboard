4471c3568e1b5a8932fa07d9b184f453f262d4d2
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-16 10:38:19 +0000

    update image src path

--- app/page.tsx
+++ app/page.tsx
@@ -34,14 +34,14 @@ export default function Page() {
                 </div>
                 <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
                     <Image
-                        src="/hero-desktop.png"
+                        src="/images/hero-desktop.png"
                         alt="Screenshots of the dashboard project showing desktop and mobile versions"
                         width={1000}
                         height={760}
                         className="hidden md:block"
                     />
                     <Image
-                        src="/hero-mobile.png"
+                        src="/images/hero-mobile.png"
                         alt="Screenshots of the dashboard project showing desktop and mobile versions"
                         width={560}
                         height={620}
