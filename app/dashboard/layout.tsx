34ecebee77807e55862e28b8a903b5c1680d3c6a
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-26 12:39:34 +0000

    update dashboard home layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,13 +6,11 @@ export default function DashboardLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-            <div className="w-full flex-none md:w-64">
-                <SideNav />
-            </div>
-            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+        <div className="flex h-screen flex-col md:flex-row-reverse md:overflow-hidden">
+            <div className="flex-grow p-6 md:overflow-y-auto lg:p-12">
                 {children}
             </div>
+            <SideNav />
         </div>
     );
 }
