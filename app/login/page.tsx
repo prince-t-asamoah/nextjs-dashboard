88eb3a769a3a12b4dfa6f6913c150de1d89dea88
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-11-27 09:55:16 +0000

    update page layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,9 +8,7 @@ export const metadata: Metadata = {
 export default function LoginPage() {
     return (
         <main className="flex items-center justify-center h-screen">
-            <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-                <LoginForm />
-            </div>
+            <LoginForm />
         </main>
     );
 }
